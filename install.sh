397db7c7e8c53c20c28773a1a0198d58909056c3
Franco Ruggeri <franco.ruggeri@ericsson.com>
2024-06-06 00:37:24 +0200

    feat: move cursor beginning/end of line macos

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
 cp complex_modifications/linux_shortcuts.json ~/.config/karabiner/assets/complex_modifications/
+cp complex_modifications/macos_shortcuts.json ~/.config/karabiner/assets/complex_modifications/
